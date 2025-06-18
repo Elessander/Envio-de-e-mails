db31a9e9cbd85baafc18ccfeb780bd12aa6ace0e
Elessander <elessanderribovski775@hotmail.com>
2025-06-18 20:02:08 -0300

    alterações nos emails reais

--- producer.js
+++ producer.js
@@ -5,21 +5,20 @@ const { faker } = require('@faker-js/faker');
 async function addJobs() {
   // E‐mails reais para teste
   const realEmails = [
-    'deathstorm751@gmail.com'
+    'insira-emails-reais@gmail.com',
   ];
 
   for (const to of realEmails) {
     await emailQueue.add('sendEmail', {
       to,
-      subject: 'Testando1!'
+      subject: 'Um video'
     }, {
       attempts: 3,
       backoff: { type: 'exponential', delay: 3000 }
     });
   }
 
-  // mais 296 aleatórios
-  for (let i = 0; i < 3; i++) {
+  for (let i = 0; i < 300; i++) {
     await emailQueue.add('sendEmail', {
       to: faker.internet.email(),
       subject: faker.lorem.sentence()
